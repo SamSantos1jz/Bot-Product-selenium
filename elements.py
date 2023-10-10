528978e18aec2d85c3d85f2e5d45a875c2a60402
SamSantos1jz <samsantos1jz@gmail.com>
2023-10-10 03:09:27 -0300

    feat: iniciando automacao

---
+++ elements.py
@@ -0,0 +1,9 @@
+
+
+input_code = "/html/body/div[1]/div/div[1]/div[2]/form/div[1]/div[1]/input"
+input_marca = "/html/body/div[1]/div/div[1]/div[2]/form/div[1]/div[2]/input"
+input_type_product = "/html/body/div[1]/div/div[1]/div[2]/form/div[1]/div[3]/input"
+input_category_product = "/html/body/div[1]/div/div[1]/div[2]/form/div[1]/div[4]/input"
+input_price = "/html/body/div[1]/div/div[1]/div[2]/form/div[1]/div[5]/input"
+input_cust = "/html/body/div[1]/div/div[1]/div[2]/form/div[1]/div[6]/input"
+input_obs = "/html/body/div[1]/div/div[1]/div[2]/form/div[1]/div[7]/input"
--- script_main.py
+++ script_main.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pyautogui
-import time
+from time import sleep
+from elements import input_code
+import pandas
 
 pyautogui.PAUSE = 0.3
 
@@ -20,10 +22,29 @@ email_input.send_keys("samteste@gmail.com")
 password_input = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/form/div[1]/div[2]/input")
 password_input.send_keys("12345")
 
+sleep(2)
+
 button_login = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/form/div[2]/button")
 button_login.click()
 
-time.sleep(10)
+# cadastrando produtos
+
+table = pandas.read_csv("product.csv")
+
+for row in table.index:
+
+    codigo = table.loc[row, "codigo"]
+    marca = table.loc[row, "marca"]
+    tipo = table.loc[row, "tipo"]
+    categoria = table.loc[row, "categoria"]
+    preco_unitario = table.loc[row, "preco_unitario"]
+    custo = table.loc[row, "custo"]
+
+    input_code_register = driver.find_element(By.XPATH, input_code)
+    input_code_register.send_keys(codigo)
 
 
-driver.quit()
+
+
+
+sleep(10)
